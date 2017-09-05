705312e07e87ca3916b2d99176b144cb2ef6a47a
nstackpole21 <nstackpole21@gmail.com>
2017-09-05 13:21:30 -0400

    Stubbed out arithmetic classes, added value members to int and bool

--- Book_EL/AST.hpp
+++ Book_EL/AST.hpp
@@ -12,11 +12,43 @@ struct expression
 struct integer : public expression
 {
     int value;
+    integer(int);
 };
 
 struct boolean : public expression
 {
     bool value;
+    boolean(bool);
+};
+
+struct addition_expr : public expression
+{
+    expression *e1;
+    expression *e2;
+};
+
+struct subtraction_expr : public expression
+{
+    expression *e1;
+    expression *e2;
+};
+
+struct multiplication_expr : public expression
+{
+    expression *e1;
+    expression *e2;
+};
+
+struct division_expr : public expression
+{
+    expression *e1;
+    expression *e2;
+};
+
+struct remainder_expr : public expression
+{
+    expression *e1;
+    expression *e2;
 };
 
 #endif
