6c6b834a9f96fbd669cc515348f2fd99357e7a68
nstackpole21 <nstackpole21@gmail.com>
2017-09-05 13:23:59 -0400

    Added constructors to arithmetic classes.

--- Book_EL/AST.hpp
+++ Book_EL/AST.hpp
@@ -25,30 +25,35 @@ struct addition_expr : public expression
 {
     expression *e1;
     expression *e2;
+    addition_expr(expression, expression);
 };
 
 struct subtraction_expr : public expression
 {
     expression *e1;
     expression *e2;
+    subtraction_expr(expression, expression);
 };
 
 struct multiplication_expr : public expression
 {
     expression *e1;
     expression *e2;
+    multiplication_expr(expression, expression);
 };
 
 struct division_expr : public expression
 {
     expression *e1;
     expression *e2;
+    division_expr(expression, expression);
 };
 
 struct remainder_expr : public expression
 {
     expression *e1;
     expression *e2;
+    remainder_expr(expression, expression);
 };
 
 #endif
