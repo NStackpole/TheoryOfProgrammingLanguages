1c4334d0d640cc4a72fbf13ee8d5657a6c9b3f32
nstackpole21 <nstackpole21@gmail.com>
2017-09-05 13:15:07 -0400

    created base expression class as well as int and bool classes

--- Book_EL/AST.hpp
+++ Book_EL/AST.hpp
@@ -1,2 +1,22 @@
 //Nathan Stackpole, 2017
 #include <iostream>
+
+#ifndef AST_HPP
+#define AST_HPP
+
+struct expression
+{
+
+};
+
+struct integer : public expression
+{
+    int value;
+};
+
+struct boolean : public expression
+{
+    bool value;
+};
+
+#endif
