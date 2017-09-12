c990a35a94e8d4f612cb2049929d9c8aa3cd33f3
Nathan Stackpole <nstackpole21@gmail.com>
2017-09-12 17:51:46 -0400

    added test for if_expression

--- Book_EL/main.cpp
+++ Book_EL/main.cpp
@@ -14,5 +14,13 @@ int main()
         std::cout << relation_test->eval() << "\n";
     }
 
+    {
+        relational_expression *relation_test = new relational_expression(op_less_than, new integer(100), new integer(20));
+
+        if_expression *if_test = new if_expression(relation_test, new arithmetic_expression(op_add, new integer(20), new integer(30)), new arithmetic_expression(op_add, new integer(200), new integer(30)));
+
+        std::cout << if_test->eval() << "\n";
+    }
+
     return 0;
 }
