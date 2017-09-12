3e6ab30e0fc8a5d0f5a8e62d847cbec9694b02db
Nathan Stackpole <nstackpole21@gmail.com>
2017-09-12 12:42:33 -0400

    created tests for relation_expression

--- Book_EL/main.cpp
+++ Book_EL/main.cpp
@@ -4,5 +4,15 @@ int main()
 {
     std::cout << "Hello.\n";
 
+    {
+        relational_expression *relation_test = new relational_expression(op_less_than, new integer(10), new integer(20));
+        std::cout << relation_test->eval() << "\n";
+    }
+    
+    {
+        relational_expression *relation_test = new relational_expression(op_less_than, new integer(100), new integer(20));
+        std::cout << relation_test->eval() << "\n";
+    }
+
     return 0;
 }
