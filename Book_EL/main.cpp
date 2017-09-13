4ce0c96f3539e63ba61fde146b5b42c5b9beb240
Nathan Stackpole <nstackpole21@gmail.com>
2017-09-13 13:53:28 -0400

    Added tests for arithmetic_expression printing in the arithmetic_expression tests

--- Book_EL/main.cpp
+++ Book_EL/main.cpp
@@ -26,27 +26,32 @@ int main()
 
     {
         arithmetic_expression *arithmetic_test = new arithmetic_expression(op_add, new integer(200), new integer(10));
-        std::cout << arithmetic_test->eval() << "\n";
+        print(arithmetic_test);
+        std::cout << " = " << arithmetic_test->eval() << "\n";
     }
 
     {
         arithmetic_expression *arithmetic_test = new arithmetic_expression(op_subtract, new integer(200), new integer(10));
-        std::cout << arithmetic_test->eval() << "\n";
+        print(arithmetic_test);
+        std::cout << " = " << arithmetic_test->eval() << "\n";
     }
 
     {
         arithmetic_expression *arithmetic_test = new arithmetic_expression(op_multiply, new integer(200), new integer(10));
-        std::cout << arithmetic_test->eval() << "\n";
+        print(arithmetic_test);
+        std::cout << " = " << arithmetic_test->eval() << "\n";
     }
 
     {
         arithmetic_expression *arithmetic_test = new arithmetic_expression(op_divide, new integer(200), new integer(10));
-        std::cout << arithmetic_test->eval() << "\n";
+        print(arithmetic_test);
+        std::cout << " = " << arithmetic_test->eval() << "\n";
     }
 
     {
         arithmetic_expression *arithmetic_test = new arithmetic_expression(op_remainder, new integer(200), new integer(10));
-        std::cout << arithmetic_test->eval() << "\n";
+        print(arithmetic_test);
+        std::cout << " = " << arithmetic_test->eval() << "\n";
     }
 
     {
