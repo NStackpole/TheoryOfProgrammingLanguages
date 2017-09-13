4f57976b945f0bb6f8d2db1e4cf4e98033d4e4f7
Nathan Stackpole <nstackpole21@gmail.com>
2017-09-13 13:24:54 -0400

    Added eval tests for each operator of the arithmetic_expression

--- Book_EL/main.cpp
+++ Book_EL/main.cpp
@@ -8,10 +8,35 @@ int main()
         relational_expression *relation_test = new relational_expression(op_less_than, new integer(10), new integer(20));
         std::cout << relation_test->eval() << "\n";
     }
-    
+
     {
         relational_expression *relation_test = new relational_expression(op_less_than, new integer(100), new integer(20));
         std::cout << relation_test->eval() << "\n";
+    }
+
+    {
+        arithmetic_expression *arithmetic_test = new arithmetic_expression(op_add, new integer(200), new integer(10));
+        std::cout << arithmetic_test->eval() << "\n";
+    }
+
+    {
+        arithmetic_expression *arithmetic_test = new arithmetic_expression(op_subtract, new integer(200), new integer(10));
+        std::cout << arithmetic_test->eval() << "\n";
+    }
+
+    {
+        arithmetic_expression *arithmetic_test = new arithmetic_expression(op_multiply, new integer(200), new integer(10));
+        std::cout << arithmetic_test->eval() << "\n";
+    }
+
+    {
+        arithmetic_expression *arithmetic_test = new arithmetic_expression(op_divide, new integer(200), new integer(10));
+        std::cout << arithmetic_test->eval() << "\n";
+    }
+
+    {
+        arithmetic_expression *arithmetic_test = new arithmetic_expression(op_remainder, new integer(200), new integer(10));
+        std::cout << arithmetic_test->eval() << "\n";
     }
 
     {
